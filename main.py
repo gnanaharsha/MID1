8f5413d7b2682d71772bfd148db55113f95ee361
Not-Yeshwanth-Reddy <peddamalluyeshwanth1999@gmail.com>
2019-09-26 16:30:37 +0530

    Main.py fixed

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import sub
 import prod
 import div
 
+
 choice = input("enter operaton:\n1-add\n2-sub\n3-prod\n4-div\nchoice:\t")
+global a, b
 a = int(input("a: "))
 b = int(input("b: "))
-if choice==1:
-	print(add(a, b))
-if choice==2:
-	print(sub(a, b))
-if choice==3:
-	print(prod(a, b))
-if choice==4:
-	print(div(a, b))
+if choice=="1":
+	ans = add.add(a, b)
+if choice=="2":
+	ans = sub.sub(a, b)
+if choice=="3":
+	ans = prod.product(a, b)
+if choice=="4":
+	ans = div.div(a, b)
+
+print(ans)
