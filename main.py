387ee8c2ec20a9bc5984828c004a287e24c1cd0a
gnanaharsha <GnanaH.Paruchuri@st.niituniversity.in>
2019-09-26 15:57:07 +0530

    updated main file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world")
+import add
+import sub
+import prod
+impor div
+
+input = input("enter operaton:\n1-add\n2-sub\n3-prod\n4-div\nchouce:\t")
+a = input("a: ")
+b = input("b: ")
+if input==1:
+	print(add(a, b))
